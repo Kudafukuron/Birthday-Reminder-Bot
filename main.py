865174948a48be9eca21ed53a2d3e30ea081ac77
Kudafukuron <105971898+Kudafukuron@users.noreply.github.com>
2023-06-20 14:25:00 +0500

    Mark IV

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot
 import uuid
 import time
-from flask import Flask, render_template
 from datetime import datetime, timedelta, timezone
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -9,11 +8,6 @@ TOKEN = '6033807042:AAGVUHYKdz_bdXDlxzYDWRHtnx1_Erx5hP4'
 
 bot = telebot.TeleBot(TOKEN)
 scheduler = BackgroundScheduler()
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return '<h1>Hello Bot!</h1>'
 
 # Our Database
 birthdays = {}
@@ -117,8 +111,6 @@ def greet(message):
   bot.send_message(message.chat.id, "Welcome Birthday Reminder I! You can store here people's birthdays without forgeting who has today a birthday! Start with /Add command to add a new person!")
 
 def main():
-  app.run()
-
   scheduler.start()
   
   bot.polling()
